8662aeec95f56026abd2490076ce9dd86511f2db
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-08 10:57:48 +0100

    Exercise 1
    This was created to display the weather for the next seven days, pratice dependency injection on a blazor page.

---
+++ IncludedSystems/DependencyInjectionDemo/Exercises1/Data/IWeatherForecastService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exercises1.Data
+{
+    public interface IWeatherForecastService
+    {
+        Task<WeatherForecast[]> GetForecastAsync(DateTime startDate, int amount = 5);
+    }
+}
---
+++ IncludedSystems/DependencyInjectionDemo/Exercises1/Data/WeatherForecastService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercises1.Data
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate, int amount = 5)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, amount).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
