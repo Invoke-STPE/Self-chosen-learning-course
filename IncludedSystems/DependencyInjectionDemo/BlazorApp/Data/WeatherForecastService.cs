19b49bc809c53fd3eb8569f29f00394e15f956c1
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-08 10:36:19 +0100

    Finished watching the Dependency Injection Video.

---
+++ IncludedSystems/DependencyInjectionDemo/BlazorApp/Data/WeatherForecastService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly IDataAccess db;
+
+        public WeatherForecastService(IDataAccess db)
+        {
+            this.db = db;
+        }
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, db.GetUserAge()).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
