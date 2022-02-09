6637e675aa37672047170fe53fde700c92a245a5
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-09 21:06:09 +0100

    Completed homework

---
+++ Pages/Homework/BlazorApp/Pages/Voting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public partial class Voting
+    {
+        private int condidate1Votes;
+        private int condidate2Votes;
+
+        private void VoteCandidate1()
+        {
+            condidate1Votes++;
+        }
+        private void VoteCandidate2()
+        {
+            condidate2Votes++;
+        }
+    }
+}
