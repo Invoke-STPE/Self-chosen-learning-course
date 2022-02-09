9ce29a9dc65d3d8ac5140d28260cef17e0f2ebf0
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-09 19:48:45 +0100

    Exercise 2
    For this exercises I change counter and fetchdata from on page code too partial class code.

---
+++ Pages/PageCodeDemo/Exercise2/Pages/FetchData.cs
@@ -0,0 +1,19 @@
+﻿using Exercise2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise2.Pages
+{
+    public partial class FetchData
+    {
+        public WeatherForecastService ForecastService { get; set; }
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
