c8cb36a796c98f7d0ac8bca7f4815a2db1375c52
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-09 19:39:30 +0100

    Finished watching Page Code Support video.

---
+++ Pages/PageCodeDemo/BlazorApp/Pages/CounterBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+
+namespace BlazorApp.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        protected int currentCount = 0;
+        [Inject] // You cannot use a constructor in a ComponentBase, since it's ran before the page. You can then Inject with this. 
+        public ILogger<CounterBase> Log { get; set; }
+        protected void IncrementCount()
+        {
+            currentCount++;
+            Log.LogInformation("The new value is {CounterValue}", currentCount);
+        }
+    }
+}
