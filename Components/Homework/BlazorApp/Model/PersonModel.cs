f809128e9b77d8cb767abc7f7a4db5c0202ccdbb
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-12 09:48:01 +0100

    Created my own solution for homework.

---
+++ Components/Homework/BlazorApp/Model/PersonModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Model
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Fullname { get; set; }
+
+        public PersonModel()
+        {
+            Fullname = $"{FirstName} {LastName}";
+        }
+        public PersonModel(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Fullname = $"{firstName} {lastName}";
+
+        }
+    }
+}
---
+++ Components/Homework/BlazorApp/Pages/Users.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public partial class Users
+    {
+        List<PersonModel> people;
+        public Users()
+        {
+            people = new List<PersonModel>()
+           {
+               new PersonModel(1, "Steven", "Pedersen"),
+               new PersonModel(2, "Mike", "Brandt"),
+               new PersonModel(3, "Kevin", "Brandt"),
+           };
+        }
+
+        private void updateName(int id, string name)
+        {
+            PersonModel person = people.SingleOrDefault(p => p.Id == id);
+            person.Fullname = name;
+
+        }
+    }
+}
