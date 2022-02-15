ff33a35fca553570acdd54585d496ecccac7c342
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-15 10:44:30 +0100

    Finished watching the video, and re did his example.
    Have to do it tonight as well.

---
+++ Forms/AdvancedFormsDemo/Exercises1/Models/MediaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercises1.Models
+{
+    public class MediaModel
+    {
+        public int FirstId { get; set; }
+        public int SecondID { get; set; }
+    }
+}
