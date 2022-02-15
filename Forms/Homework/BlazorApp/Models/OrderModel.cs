6fc6f2d23bbd9c420d7acab4b4060440be5f559e
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-15 13:11:55 +0100

    Homework completed

---
+++ Forms/Homework/BlazorApp/Models/OrderModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class OrderModel
+    {
+        [Required]
+        public int MilkshakeType { get; set; }
+        [Required]
+        public int MilkshakeSize { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+        public bool WhippedCream { get; set; }
+    }
+}
