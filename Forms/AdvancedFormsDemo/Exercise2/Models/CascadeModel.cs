cade0856d544eb449e93b5d817ff57917578f2b5
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-16 07:55:12 +0100

    Exercises 2
    Solidified lesson learned.

---
+++ Forms/AdvancedFormsDemo/Exercise2/Models/CascadeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise2.Models
+{
+    public class CascadeModel
+    {
+        public int FirstId { get; set; }
+        public int SecondID { get; set; }
+    }
+}
