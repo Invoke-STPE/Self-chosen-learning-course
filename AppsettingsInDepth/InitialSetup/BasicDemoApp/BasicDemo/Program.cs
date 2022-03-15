b0737373d97bcc9790703194d0471028b5b4b993
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-03-15 14:25:45 +0100

    Update Program.cs

--- AppsettingsInDepth/InitialSetup/BasicDemoApp/BasicDemo/Program.cs
+++ AppsettingsInDepth/InitialSetup/BasicDemoApp/BasicDemo/Program.cs
@@ -20,6 +20,24 @@ namespace BasicDemo
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, builder) =>
+                {
+                    IHostEnvironment env = hostingContext.HostingEnvironment;
+                    builder.Sources.Clear();
+                    // How you add jsons file are important
+                    // The first is the last place to look
+                    builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    builder.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true); // This is how we get the specific development enviroments.
+
+                    if (hostingContext.HostingEnvironment.IsDevelopment())
+                    {
+                        // This will look after usersecrets, and add if it's true.
+                        // Never do it for production, you do not need it.
+                        builder.AddUserSecrets<Program>();
+                    }
+                    builder.AddEnvironmentVariables();
+                    builder.AddCommandLine(args);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
